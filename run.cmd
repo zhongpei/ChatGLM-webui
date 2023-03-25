c72b64472818435d58e9d217a25f969e50a1918b
fofo <zhongpei@vip.qq.com>
2023-03-25 15:26:04 +0800

    add int4-qe option

---
+++ run.cmd
@@ -0,0 +1,13 @@
+@echo off
+cd /D "%~dp0"
+
+
+:start
+echo "Start VENV"
+call .venv\Scripts\activate.bat
+goto :run
+
+:run
+echo "Start WebUI"
+python webui.py --model-path ./cache --precision int4-qe
+pause
